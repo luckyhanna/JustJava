fe1073207940a934b546079f67d95944cc4a5d0b
Hanna <hanna.botar@teamnet.ro>
2017-11-28 19:45:32 +0200

    Added minus and plus buttons + methods.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.justjava;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,13 +14,16 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
-    float plainCoffeePrice = 5.25f;
+    int quantity = 0;
+    float plainCoffeePrice = 5f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButtonEnabled(R.id.minus_button, false);
+        display(quantity);
+        displayPrice(quantity * plainCoffeePrice);
     }
 
     /**
@@ -44,5 +49,39 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
 //        priceTextView.setText("$" + number);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void incrementQuantity(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+        displayPrice(quantity * plainCoffeePrice);
+        if (quantity == 1) {
+            setButtonEnabled(R.id.minus_button, true);
+        }
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrementQuantity(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+        displayPrice(quantity * plainCoffeePrice);
+        if (quantity == 0) {
+            setButtonEnabled(R.id.minus_button, false);
+        }
+    }
+
+    /**
+     * This methos enables/disables a button
+     * @param id - id of the button
+     * @param enabled - the state you want to put the button in
+     */
+    private void setButtonEnabled(@IdRes int id, boolean enabled) {
+        Button button = (Button) findViewById(id);
+        button.setEnabled(enabled);
     }
 }
