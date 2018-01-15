d17ba9861b3b036404842577a73e9fbbd674edfa
Hanna <hanna.botar@teamnet.ro>
2018-01-15 19:29:20 +0200

    Added background

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -31,28 +31,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO check how this might work - scaling needed!!!
+//        getWindow().setBackgroundDrawableResource(R.drawable.background1);
+
         setButtonEnabled(R.id.minus_button, false);
         setButtonEnabled(R.id.order_button, false);
         displayQuantity(quantity);
-        // update price too
-//        displayPrice(quantity * plainCoffeePrice);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        hasWhippedCream = whippedCreamCheckBox.isChecked();
-        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        hasChocolate = chocolateCheckBox.isChecked();
+        // Get the user's name and check if it has been provided
         EditText nameInput = (EditText) findViewById(R.id.name_input);
         myName = nameInput.getText().toString();
+        if (myName.length() == 0) {
+            Toast nameMissingToast = Toast.makeText(this, getString(R.string.name_missing), Toast.LENGTH_LONG);
+            nameMissingToast.show();
+            return;
+        }
+
+        // Check if the user wants whipped cream
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        hasWhippedCream = whippedCreamCheckBox.isChecked();
+        // Check if the user wants chocolate
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        hasChocolate = chocolateCheckBox.isChecked();
 
         float price = calculatePrice();
         String orderSummary = createOrderSummary(price);
 
-//        displayMessage(orderSummary);
         // send email
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:"));
@@ -63,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // NOT USED
     private void showMap() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("geo:47.6, -122.3"));
@@ -103,15 +114,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given text on the screen
-     * @param message - the text to show
-     */
-    private void displayMessage(String message) {
-        /*TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);*/
-    }
-
-    /**
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int quantity) {
@@ -125,8 +127,6 @@ public class MainActivity extends AppCompatActivity {
     public void incrementQuantity(View view) {
         quantity = quantity + 1;
         displayQuantity(quantity);
-        // update price too
-//        displayPrice(quantity * plainCoffeePrice);
         if (quantity == 1) {
             setButtonEnabled(R.id.minus_button, true);
             setButtonEnabled(R.id.order_button, true);
@@ -143,8 +143,6 @@ public class MainActivity extends AppCompatActivity {
     public void decrementQuantity(View view) {
         quantity = quantity - 1;
         displayQuantity(quantity);
-        // update price too
-//        displayPrice(quantity * plainCoffeePrice);
         if (quantity == 0) {
             setButtonEnabled(R.id.minus_button, false);
             setButtonEnabled(R.id.order_button, false);
